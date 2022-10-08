fbb1bcacf5c6ee4adcbac29645ce30b0fd7cc8e7
Akira <96494621+Akira61@users.noreply.github.com>
2022-10-08 23:38:58 +0300

    Update client.js

--- sexy-chat/client/client.js
+++ sexy-chat/client/client.js
@@ -3,8 +3,6 @@ const dev = document.getElementById('dev');
 const messages = document.getElementById('messages');
 const input = document.getElementById('input');
 
-const name = prompt('Please enter your name:');
-exports.username = name;
 socket.on('message', (msg) =>{
     let item = document.createElement('li');
     item.innerText = msg;
